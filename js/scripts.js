169dc931276b1d89a113c453019877e06f562323
Justin Kerntz and Stuart McKay <stuart@>
2019-04-08 10:16:57 -0700

    add search by Id prototype method

--- js/scripts.js
+++ js/scripts.js
@@ -5,13 +5,30 @@ function Destination(location, landmarks, timeOfYear, notes) {
   this.timeOfYear = timeOfYear,
   this.notes = notes,
 };
-
-// Destination.prototype.addDestination = function() {
-//
-// }
+//Inintialize destination objects
+Destination.prototype.addDestinations = function(destinations) {
+  destinations.id = this.addIdToDestination();
+  this.destinations.push(destinations)
+}
+//add order of the destination that was visited
+destination.prototype.addIdToDestination = function() {
+  this.orderVisited++;
+  return this.currentId;
+}
+// Will search through destinations by assigned Id and return any matched value
+TravelLog.prototype.findDestination = function(id) {
+  for (var i = 0; i < this.destinations.length; i++) {
+    if (this.destinations[i]) {
+      if (this.destinations[i].id === id) {
+        return this.destinations[i];
+      }
+    }
+  };
+  return false;
+}
 
 // Back-end for our travel Log
 function TravelLog() {
   this.destinations = [],
-  this.currentId = 0
+  this.orderVisited = 0
 };
