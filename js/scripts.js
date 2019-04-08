3d4fe66ef49a02a4c35a8633f93ccef0764fc418
Justin Kerntz and Stuart McKay <stuart@>
2019-04-08 14:52:17 -0700

    add functionality for user to input new destinations to travel log

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,24 @@
+// Test destinations
+var destination = new Destination("China", ['Great wall of China', "Emporor's Palace"], "Monsoon Season", "The air-quality sucks", "img/Forbidden-city.jpg");
+
 //Back-end for individual destinations
-function Destination(location, landmarks, timeOfYear, notes) {
-  this.location = location,
-  this.landmarks = landmarks,
-  this.timeOfYear = timeOfYear,
-  this.notes = notes,
+function Destination(location, landmarks, timeOfYear, notes, img) {
+  this.location = location;
+  this.landmarks = landmarks;
+  this.timeOfYear = timeOfYear;
+  this.notes = notes;
+  this.img = img;
+  this.id = 0;
 };
 //Inintialize destination objects
-Destination.prototype.addDestinations = function(destinations) {
-  destinations.id = this.addIdToDestination();
-  this.destinations.push(destinations)
+TravelLog.prototype.addDestinations = function(destination) {
+  destination.id = this.addIdToDestination();
+  this.destinations.push(destination);
 }
 //add order of the destination that was visited
-destination.prototype.addIdToDestination = function() {
+TravelLog.prototype.addIdToDestination = function() {
   this.orderVisited++;
-  return this.currentId;
+  return this.orderVisited;
 }
 // Will search through destinations by assigned Id and return any matched value
 TravelLog.prototype.findDestination = function(id) {
@@ -27,12 +32,25 @@ TravelLog.prototype.findDestination = function(id) {
   return "https://en.wikipedia.org/wiki/" + this.location; // returns wikipedia link for a specific searched location if no matching location is found
 }
 // return output of travel destination during a specific time of timeOfYear
-Destination.prototype.tripDetail = function() {
-  return "I traveled to " + this.destination + " during " + this.timeOfYear ". I had a great time!"
-}
+// Destination.prototype.tripDetail = function() {
+//   return ("I traveled to " + this.destination + " during " + this.timeOfYear ". I had a great time!")
+// }
 
 // Back-end for our travel Log
 function TravelLog() {
-  this.destinations = [],
-  this.orderVisited = 0
+  this.destinations = [];
+  this.orderVisited = 0;
 };
+
+// User Interface Logic
+$(document).ready(function() {
+  var travelLog = new TravelLog();
+  $("form").submit(function(event)  {
+    event.preventDefault();
+    var destination = new Destination($("#location").val(),$("#landmarks").val(),$("#timeOfYear").val(),$("#notes").val(),$("#image").val());
+    travelLog.addDestinations(destination);
+    console.log(travelLog);
+
+  })
+
+});
